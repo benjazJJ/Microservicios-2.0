e479ab3552710688c9a45aec389f6fa329f647fe
benjazJJ <be.palmap@duocuc.cl>
2025-06-22 20:26:32 -0400

    Añadimos microservicio de prestamos

---
+++ Microservicio Prestamos/src/main/java/Servicio/Microservicio/Prestamos/MicroservicioPrestamosApplication.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.Prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPrestamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPrestamosApplication.class, args);
+	}
+
+}
---
+++ Microservicio Prestamos/src/test/java/Servicio/Microservicio/Prestamos/MicroservicioPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package Servicio.Microservicio.Prestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPrestamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
